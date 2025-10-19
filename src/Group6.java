13dadd861b0539bca20609d8bb0dcca7c6e47566
thegreenn1nja <73561889+thegreenn1nja@users.noreply.github.com>
2025-10-19 18:58:44 -0500

    Implemented Quicksort
    Implemented quicksort using 2 pivots, median of 3 pivot selection, and using insertion sort for inside sub arrays while quicksort is being used.

--- src/Group6.java
+++ src/Group6.java
@@ -2,9 +2,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
@@ -41,7 +39,7 @@ public class Group6 {
 		String[] data = input.toArray(new String[0]); // read data as strings
 		
 		// Test print
-		System.out.println("The target string is: " + target);
+		// System.out.println("The target string is: " + target);
 
 		String[] toSort = data.clone(); // clone the data
 		
@@ -95,8 +93,7 @@ public class Group6 {
 	 GROUP 6 Jakob&John ALGORITHM: 
 	 Thinking about using a modified quick sort algorithm, it follows 
 	 assignment constraints on memory cap. Complexity for average case is
-	 O(n log n) which will be effective and fast in sorting. Commented out bits 
-	 is our starter code for quicksort. 
+	 O(n log n) which will be effective and fast in sorting. 
 	*/
 	
 	private static void sort(String[] toSort, String target) {
@@ -109,61 +106,124 @@ public class Group6 {
 		// If you are creating or modifying any global variables, you need to reset them 
 		// to the original state after your sorting is done.  
 		
-		// This sorting gives you the correct sorted order:
-		Arrays.sort(toSort, new SortingCompetitionComparator(target));
-		
-		/* 
-		if (low < high) {
-            int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
-			*/
+		// Use an in-place quicksort with the competition comparator.
+		if (toSort == null || toSort.length <= 1) return;
+		SortingCompetitionComparator comp = new SortingCompetitionComparator(target);
+		quicksort(toSort, 0, toSort.length - 1, comp);
+	}
+    
+	// Dual-pivot quicksort helpers (using two pivots -> three partitions)
+	private static final int INSERTION_SORT_THRESHOLD = 16;
+
+	private static void quicksort(String[] arr, int low, int high, SortingCompetitionComparator comp) {
+		while (low < high) {
+			int len = high - low + 1;
+			// Use insertion sort for small partitions
+			if (len <= INSERTION_SORT_THRESHOLD) {
+				insertionSort(arr, low, high, comp);
+				return;
+			}
+
+			// median-of-three to choose better pivots: pick low, mid, high
+			int mid = low + (high - low) / 2;
+			// Order low, mid, high
+			if (comp.compare(arr[low], arr[mid]) > 0) swap(arr, low, mid);
+			if (comp.compare(arr[low], arr[high]) > 0) swap(arr, low, high);
+			if (comp.compare(arr[mid], arr[high]) > 0) swap(arr, mid, high);
+
+			// Use arr[low] and arr[high] as pivots (now better chosen)
+			if (comp.compare(arr[low], arr[high]) > 0) swap(arr, low, high);
+
+			String pivot1 = arr[low];
+			String pivot2 = arr[high];
+
+			int lt = low + 1; // boundary for elements < pivot1
+			int gt = high - 1; // boundary for elements > pivot2
+			int i = low + 1;
+
+			while (i <= gt) {
+				int cmpToP1 = comp.compare(arr[i], pivot1);
+				if (cmpToP1 < 0) {
+					swap(arr, i, lt);
+					lt++;
+					i++;
+				} else {
+					int cmpToP2 = comp.compare(arr[i], pivot2);
+					if (cmpToP2 > 0) {
+						swap(arr, i, gt);
+						gt--;
+					} else {
+						i++;
+					}
+				}
+			}
+
+			// place pivots to their correct positions
+			swap(arr, low, lt - 1);
+			swap(arr, high, gt + 1);
+
+			int p1 = lt - 1;
+			int p2 = gt + 1;
+
+			// Recurse on smaller partitions first to limit stack depth
+			int leftSize = p1 - 1 - low;
+			int middleSize = p2 - 1 - (p1 + 1) + 1; // p2 - p1 - 1
+			int rightSize = high - (p2 + 1) + 1;
+
+			// sort three partitions: [low, p1-1], [p1+1, p2-1], [p2+1, high]
+			// Recurse on smallest, loop on the largest to reduce recursion depth
+			if (leftSize <= middleSize && leftSize <= rightSize) {
+				quicksort(arr, low, p1 - 1, comp);
+				if (middleSize <= rightSize) {
+					quicksort(arr, p1 + 1, p2 - 1, comp);
+					low = p2 + 1;
+				} else {
+					quicksort(arr, p2 + 1, high, comp);
+					low = p1 + 1;
+					high = p2 - 1;
+				}
+			} else if (middleSize <= leftSize && middleSize <= rightSize) {
+				quicksort(arr, p1 + 1, p2 - 1, comp);
+				if (leftSize <= rightSize) {
+					quicksort(arr, low, p1 - 1, comp);
+					low = p2 + 1;
+				} else {
+					quicksort(arr, p2 + 1, high, comp);
+					high = p1 - 1;
+				}
+			} else {
+				quicksort(arr, p2 + 1, high, comp);
+				if (leftSize <= middleSize) {
+					quicksort(arr, low, p1 - 1, comp);
+					low = p1 + 1;
+					high = p2 - 1;
+				} else {
+					quicksort(arr, p1 + 1, p2 - 1, comp);
+					high = p1 - 1;
+				}
+			}
+		}
 	}
 
-	/*
-	 private static int partition(TestInteger[] arr, int low, int high) {
-        TestInteger pivot = arr[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j].compareTo(pivot) <= 0) {
-                i++;
-                TestInteger temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        TestInteger temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-        return i + 1;
-    }
-    int partition(int a[], int low, int high)
-    {
-        int pivot = a[high]; 
-        int i = (low-1);
-        for (int j=low; j<high; j++)
-        {
-          
-            // If current element is smaller than or
-            // equal to pivot
-            if (a[j] <= pivot)
-            {
-                i++;
+	private static void insertionSort(String[] arr, int low, int high, SortingCompetitionComparator comp) {
+		for (int i = low + 1; i <= high; i++) {
+			String key = arr[i];
+			int j = i - 1;
+			while (j >= low && comp.compare(arr[j], key) > 0) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = key;
+		}
+	}
 
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-            }
-        }
-
-        int temp = a[i+1];
-        a[i+1] = a[high];
-        a[high] = temp;
-
-        return i+1;
-    }
-		*/
+	private static void swap(String[] arr, int i, int j) {
+		if (i == j) return;
+		String tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+	
 
 	private static class SortingCompetitionComparator implements Comparator<String> {
 		private String target;
@@ -179,16 +239,13 @@ public class Group6 {
 			int count1 = distanceToTarget(s1);			
 			int count2 = distanceToTarget(s2);
 			
-			// second criterion: the value of the binary numbers in the strings:
-			BigInteger n1 = new BigInteger(s1, 2); // converting a binary number into a BigInteger
-			BigInteger n2 = new BigInteger(s2, 2); // converting a binary number into a BigInteger
-			
+			// second criterion: lexicographic compare of binary strings (same length)
 			if (count1 - count2 != 0) { // if the difference from the target string aren't the same 
 				return (count1 - count2); // return a number < 0 if the first string is closer, > 0 if the second one is
 			}
-			
-			// if the two counts are the same
-			return n1.compareTo(n2);
+
+			// if the two counts are the same, lexicographic compare equals numeric compare for equal-length binary strings
+			return s1.compareTo(s2);
 
 		}
 		
@@ -211,30 +268,30 @@ public class Group6 {
 			return count;
 		}
 		
-		private static void runComparatorTests() {
-			// creating an instance of a comparator with a mock target string:
-			SortingCompetitionComparator strComp = new SortingCompetitionComparator("0000000000");
+		// private static void runComparatorTests() {
+		// 	// creating an instance of a comparator with a mock target string:
+		// 	SortingCompetitionComparator strComp = new SortingCompetitionComparator("0000000000");
 			
 			
-			// Testing distance to target
-			System.out.println("distanceToTarget(\"1010101010\") = " + strComp.distanceToTarget("1010101010")); // should be 5
-			System.out.println("distanceToTarget(\"1111101111\") = " + strComp.distanceToTarget("1111101111")); // should be 9
+		// 	// Testing distance to target
+		// 	System.out.println("distanceToTarget(\"1010101010\") = " + strComp.distanceToTarget("1010101010")); // should be 5
+		// 	System.out.println("distanceToTarget(\"1111101111\") = " + strComp.distanceToTarget("1111101111")); // should be 9
 			
-			// Testing the comparator:
-			System.out.println(strComp.compare("1010000000","0010101000")); // different distance from the target, 1st string smaller
-			System.out.println(strComp.compare("1010011001","0010101000")); // different distance from the target, 1st string larger
-			System.out.println(strComp.compare("1010000000","0010001000")); // same distance from target string, 1st string larger
-			System.out.println(strComp.compare("1010000000","1110001000")); // same distance from target string, 1st string smaller
-			System.out.println(strComp.compare("1010000000","1010000000")); // string equals to itself
+		// 	// Testing the comparator:
+		// 	System.out.println(strComp.compare("1010000000","0010101000")); // different distance from the target, 1st string smaller
+		// 	System.out.println(strComp.compare("1010011001","0010101000")); // different distance from the target, 1st string larger
+		// 	System.out.println(strComp.compare("1010000000","0010001000")); // same distance from target string, 1st string larger
+		// 	System.out.println(strComp.compare("1010000000","1110001000")); // same distance from target string, 1st string smaller
+		// 	System.out.println(strComp.compare("1010000000","1010000000")); // string equals to itself
 			
-			// Testing real-test 100 character strings; all comparisons should come out negative
-			SortingCompetitionComparator strComp1 = new SortingCompetitionComparator("0110011111000000110010001001111100001100000100111101011101100110101000011010000100101010011011100110");
-			System.out.println(strComp1.compare("0100010011000001111100001011110100000100011101111001010100100110110010101011000101010010001111100100",
-					"0100010011000001111100001011110100000100011101111001010100100110110010101011000101010010001110100100"));
-			System.out.println(strComp1.compare("0010001101010101111111000011110101000100101100011101011111101001100011111000000000101010011010101000",
-					"0110011001010000100110111010110010000000011001111101010000010010101001011000000110110010011101101110"));
+		// 	// Testing real-test 100 character strings; all comparisons should come out negative
+		// 	SortingCompetitionComparator strComp1 = new SortingCompetitionComparator("0110011111000000110010001001111100001100000100111101011101100110101000011010000100101010011011100110");
+		// 	System.out.println(strComp1.compare("0100010011000001111100001011110100000100011101111001010100100110110010101011000101010010001111100100",
+		// 			"0100010011000001111100001011110100000100011101111001010100100110110010101011000101010010001110100100"));
+		// 	System.out.println(strComp1.compare("0010001101010101111111000011110101000100101100011101011111101001100011111000000000101010011010101000",
+		// 			"0110011001010000100110111010110010000000011001111101010000010010101001011000000110110010011101101110"));
 			
-		}
+		// }
 	}
 
 	private static void writeOutResult(String[] sorted, String outputFilename) throws FileNotFoundException {
